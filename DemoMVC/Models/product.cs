4e7d34b4d478bfced5a00e6094eb494601bbfdd6
hieunguyenvunnpt-bit <hieunguyenvu.nnpt@gmail.com>
2026-03-03 18:00:15 +0700

    DemoMVC

---
+++ DemoMVC/Models/product.cs
@@ -0,0 +1,11 @@
+namespace DemoMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public double Price { get; set; }
+    }
+}
